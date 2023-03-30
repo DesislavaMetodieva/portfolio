7c12e1cc5349a95198fdf1d39657ab723658e410
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-03-30 21:37:56 +0100

    Added Project data json

---
+++ src/data.js
@@ -0,0 +1,60 @@
+// Needs update on the image data for each project
+// Potentially remove description and repoLink
+export const projects = [
+  {
+    id: 0,
+    title: "Meal Master",
+    repoLink: "https://github.com/DesislavaMetodieva/meal-master",
+    description: "A recipe search engine",
+    image: "./bootstrap-portfolio.jpg",
+    link: "https://delightful-granita-074cbc.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "My React portfolio",
+    repoLink: "https://github.com/DesislavaMetodieva/my-react-portfolio",
+    description: "First portfolio built with React",
+    image: "./bootstrap-portfolio.jpg",
+    link: "https://spontaneous-tulumba-8c21cf.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "My Bootstrap portfolio",
+    repoLink: "https://github.com/DesislavaMetodieva/Bootstrap-Portfolio",
+    description: "My project portfolio built with Bootstrap",
+    image: "./bootstrap-portfolio.jpg",
+    link: "https://desislavametodieva.github.io/Bootstrap-Portfolio/",
+  },
+  {
+    id: 3,
+    title: "The Daily Planner App",
+    repoLink: "https://github.com/DesislavaMetodieva/daily-planner-app",
+    description: "Calendar application that allows saving events",
+    image: "./daily-planner.jpg",
+    link: "https://desislavametodieva.github.io/daily-planner-app/",
+  },
+  {
+    id: 4,
+    title: "Password generator app",
+    repoLink: "https://github.com/DesislavaMetodieva/Password-generator",
+    description: "App which generates random password",
+    image: "./password-generator.jpg",
+    link: "https://desislavametodieva.github.io/Password-generator/",
+  },
+  {
+    id: 5,
+    title: "MoodMovies",
+    repoLink: "https://github.com/heirfanahmed/mood-movies",
+    description: "A weather app which recommends movies based on the current weather",
+    image: "./mood-movies.jpg",
+    link: "https://heirfanahmed.github.io/mood-movies/",
+  },
+  {
+    id: 6,
+    title: "README generator app",
+    repoLink: "https://github.com/DesislavaMetodieva/readme-generator",
+    description: "Command-line application that dynamically generates a README file",
+    image: "./readme-generator.jpg",
+    link: "https://github.com/DesislavaMetodieva/readme-generator",
+  }
+];
