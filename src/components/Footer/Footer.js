29ad95c3ae817ca75d2d14246c8fe98bbd4af946
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-04-01 13:54:01 +0100

    Added Footer and Wrapper boilerplate

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+      <>
+          <p>&copy; Copyright 2023 | Made in rainy London</p>
+      </>
+    );
+  };
+  
+  export default Footer;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Hero.css'
+
+const Hero = () => {
+    return (
+      <h2>Hello World</h2>
+    );
+  };
+  
+  export default Hero;
