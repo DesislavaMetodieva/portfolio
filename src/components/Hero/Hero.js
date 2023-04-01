4d5f7aa5fe5eb4eeec4b31e7dc443b714b2a3545
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-04-01 15:21:19 +0100

    Added text to Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './Hero.css'
+import { FaArrowDown } from 'react-icons/fa';
 
 const Hero = () => {
     return (
-         <div>
-          <h2>DESI_</h2>
-          <h2>__METODIEVA</h2>
-        </div>
+         <section className="titles">
+            <div>
+                <h2>DESI_</h2>
+                <h2>__METODIEVA</h2>
+            </div>
+                <h3>/ WEB DEVELOPER</h3>
+                <h4>/ LONDON UK <FaArrowDown className="arrow-icon"/></h4>
+        </section>
     );
   };
   
