e8d5275ecbf161f53c0924bbc1e3a5ee3ed9bd32
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-04-03 22:10:55 +0100

    Removed Wrapper component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Hero from './components/Hero/Hero'
-import Wrapper from './components/Wrapper/Wrapper'
+// import Wrapper from './components/Wrapper/Wrapper'
 import Social from './components/Social/Social';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <>
-    <Wrapper>
+    <div className="app">
+   
       <Hero />
-    </Wrapper>
-    <Social />
-    </>
+      <Social />
+      <Footer />
+    
+    </div>
   );
 }
 
--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Social.css'
-// import GitHub from './github.svg'
+import Link from '@mui/joy/Link'
+import GitHub from './github.svg'
 // import LinkedIn from './linkedin.svg'
 // import Twitter from './twitter.svg'
 
@@ -8,9 +9,11 @@ const Social = () => {
     return (
         
     <div className="social-container">
-        <div style={{ borderTop: "3px solid #fff ", marginLeft: 20, marginRight: 20 }}>
+        <div style={{ borderTop: "2px solid #fff " }}>
        </div>  
-     
+       <Link href="https://github.com/DesislavaMetodieva" target="_blank" rel="noopener" >
+          <img src={GitHub} alt={GitHub} />
+        </Link>
          
     </div>
        
