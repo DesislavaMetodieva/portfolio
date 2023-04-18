95f31c9a5fb2183de8ec9bddfc4e5c9022738bbf
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-04-18 19:59:32 +0100

    Added hr element

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,18 +4,20 @@ import ProfilePicture from './desi.jpg'
 
 const Hero = () => {
     return (
-         <section className="titles">
-            <div className="titles-items">
-                <h2>DESI_</h2>
-                <h2>__METODIEVA</h2>
-                <h3>/ WEB DEVELOPER</h3>
-                <h4>/ LONDON UK ↓</h4>
-            </div>
-            <div className="hero-image">
-        <img src={ProfilePicture} alt={"desi-author-face"} />
-      </div>
-      <hr></hr>
-        </section>
+         <><section className="titles">
+        <div className="titles-items">
+          <h2>DESI_</h2>
+          <h2>__METODIEVA</h2>
+          <h3>/ WEB DEVELOPER</h3>
+          <h4>/ LONDON UK ↓</h4>
+        </div>
+        <div className="hero-image">
+          <img src={ProfilePicture} alt={"desi-author-face"} />
+        </div>
+      </section>
+          <hr />
+       </>
+         
     );
   };
   
