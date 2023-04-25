680770c741e1ea6657a7480348eb5020740393f1
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-04-25 21:49:11 +0100

    Fixed alignment on project container

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -6,14 +6,14 @@ import { projects } from "./data.js"
 const ProjectPage = () => {
   const listingProjects = projects.map ((project) => (
     <div className="project-container" key={project.id}>
-      <div className="project-box">
-        <p className="index">[/></p>
-        <p className="project-listing">{project.id}</p>
-        <h6>░ ░ ░ {project.title} ░ ░ ░</h6>
-        <a className="link-repo" href={project.repoLink} target="_blank" rel="noreferrer">
-          Visit site <span><img src={triangle} alt="Triangle icon" /> → </span>
-        </a>
-      </div>  
+      <p className="index-sign">[/></p>
+        <div className="project-box">
+          <p className="project-index">{project.id}</p>
+          <p className="title"><span className="shaded-block">░ ░ ░</span> {project.title} <span className="shaded-block">░ ░ ░</span></p>
+          <a className="link-repo" href={project.repoLink} target="_blank" rel="noreferrer">
+            Visit site <span><img src={triangle} alt="Triangle icon" /> → </span>
+          </a>
+        </div>  
     </div> 
   ))
   return (
