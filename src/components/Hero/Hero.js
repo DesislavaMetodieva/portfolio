2acb43a622c80ad725565b7ff4f6a7ab1ebda63a
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-04-25 22:47:59 +0100

    Added bio

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,6 +9,7 @@ const Hero = () => {
           <h2>DESI_</h2>
           <h2>__METODIEVA</h2>
           <h3>/ WEB DEVELOPER</h3>
+          <p className="bio">As a web developer, I specialize in building websites and web applications using various programming languages and frameworks. I am skilled in creating responsive designs, optimizing website performance, and integrating APIs. My focus is on creating visually appealing and user-friendly websites that provide an excellent user experience.</p>
           <h4>/ LONDON UK ↓</h4>
         </div>
         <div className="hero-image">
