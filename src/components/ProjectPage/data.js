59ceb56a0f9a99431527f423eadc981f3a90b2c0
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-04-28 16:43:46 +0100

    Added stack details to data.js

--- src/components/ProjectPage/data.js
+++ src/components/ProjectPage/data.js
@@ -5,35 +5,47 @@ export const projects = [
     title: "MEAL MASTER",
     repoLink: "https://github.com/DesislavaMetodieva/meal-master",
     image: "./bootstrap-portfolio.jpg",
+    summary: "React app that allows users to search for recipes based on keywords and dietary preferences.",
+    stack: ["react", "material-ui", "api", "axios"]
   },
   {
     id: 1,
     title: "README GENERATOR",
     repoLink: "https://github.com/DesislavaMetodieva/readme-generator",
     image: "./bootstrap-portfolio.jpg",
+    summary: "This is a command-line application that dynamically generates a professional README.md file from a user's input.",
+    stack: ["javascript", "node.js", "inquirer"]
   },
   {
     id: 2,
     title: "MY BOOTSTRAP PORTFOLIO",
     repoLink: "https://github.com/DesislavaMetodieva/Bootstrap-Portfolio",
     image: "./bootstrap-portfolio.jpg",
+    summary: "This portfolio website was built using HTML, CSS, and Bootstrap, showcasing my skills and projects in an organized and visually appealing way.",
+    stack: ["html", "css", "bootstrap"]
   },
   {
     id: 3,
     title: "THE DAILY PLANNER APP",
     repoLink: "https://github.com/DesislavaMetodieva/daily-planner-app",
     image: "./daily-planner.jpg",
+    summary: "This is a calendar application that allows a user to save events for each hour of their business day.",
+    stack: ["javascript", "momentjs", "jquery", "css"]
   },
   {
     id: 4,
     title: "PASSWORD GENERATOR",
     repoLink: "https://github.com/DesislavaMetodieva/Password-generator",
     image: "./password-generator.jpg",
+    summary: "This is an application that generates a password based on criteria collected during series of prompts presented to the end-user. Once the end-user picks their criteria, the app writes the generated password on the page.",
+    stack: ["javascript", "css", "html"]
   },
   {
     id: 5,
     title: "MOOD MOVIES",
     repoLink: "https://github.com/heirfanahmed/mood-movies",
     image: "./mood-movies.jpg",
+    summary: "The app is about movies being recommended based on the present weather condition of selected city.",
+    stack: ["javascript", "api", "jquery", "css"]
   }
 ];
